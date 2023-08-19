91ace014ef5a7a4eaf84dc3542636a8866b11c95
Sinople <fab.lallemand@gmail.com>
2023-08-19 12:27:10 +0200

    TEST

--- core/php/homecenter.inc.php
+++ core/php/homecenter.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-/* This file is part of Jeedom. Beta
+/* This file is part of Jeedom. Alpha
 *
 * Jeedom is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
